1324bab465c8ec26ac4a932f3d8ff33610133046
m.m <m3farahani1382@gmail.com>
2022-08-02 16:23:11 +0430

    initial commit

---
+++ src/main/kotlin/com/example/mazidrestapi/MazidRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mazidrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MazidRestApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MazidRestApiApplication>(*args)
+}
